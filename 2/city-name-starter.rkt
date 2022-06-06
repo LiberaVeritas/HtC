e4ae3278fb6ea99c71e5f83089e4b0c9bf73e506
LiberaVeritas <korwynkim@gmail.com>
2022-06-05 21:10:22 -0700

    Create city-name-starter.rkt

---
+++ 2/city-name-starter.rkt
@@ -0,0 +1,24 @@
+
+;; city-name-starter.rkt
+
+; 
+; PROBLEM:
+; 
+; Imagine that you are designing a program that, among other things, 
+; has information about the names of cities in its problem domain.
+; 
+; Design a data definition to represent the name of a city. 
+;    
+
+
+;; CityName is String
+;; interp. name of a city
+
+(define vancouver "Vancouver")
+(define seattle "Seattle")
+
+#;
+(define (fn-for-cityname cn)
+  (... cn))
+
+;; Temple rule: atomic non-distince: String
