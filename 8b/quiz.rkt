a777a3f4de4b5c8c1d2d0a5a500c0d8b3a2b6c42
LiberaVeritas <korwynkim@gmail.com>
2022-06-22 20:33:58 -0700

    Create quiz.rkt

---
+++ 8b/quiz.rkt
@@ -0,0 +1,158 @@
+;; local-design-quiz.rkt
+
+
+; Problem 1:
+; 
+; Suppose you have rosters for players on two opposing tennis team, and each
+; roster is ordered by team rank, with the best player listed first. When both 
+; teams play, the best players of each team play one another,
+; and the second-best players play one another, and so on down the line. When
+; one team has more players than the other, the lowest ranking players on
+; the larger team do not play.
+; 
+; Design a function that consumes two rosters, and produces true if all players 
+; on both teams will play if the teams play each other. 
+; No marks will be given to solution that do not use a cross product table. 
+; 
+
+
+;; Player is String
+;; interp.  the name of a tennis player.
+(define P0 "Maria")
+(define P2 "Serena")
+
+#;
+(define (fn-for-player p)
+  (... p))
+
+
+
+;; Roster is one of:
+;; - empty
+;; - (cons Player Roster)
+;; interp.  a team roster, ordered from best player to worst.
+(define R0 empty)
+(define R1 (list "Eugenie" "Gabriela" "Sharon" "Aleksandra"))
+(define R2 (list "Maria" "Nadia" "Elena" "Anastasia" "Svetlana"))
+
+#;
+(define (fn-for-roster r)
+  (cond [(empty? r) (...)]
+        [else 
+         (... (fn-for-player (first r))
+              (fn-for-roster (rest r)))]))
+
+
+
+(define-struct match (p1 p2))
+;; Match is (make-match Player Player)
+;; interp.  a match between player p1 and player p2, with same team rank
+(define M0 (make-match "Eugenie" "Maria"))
+(define M1 (make-match "Gabriela" "Nadia"))
+
+#;
+(define (fn-for-match m)
+  (... (match-p1 m) (match-p2 m)))
+
+
+
+;; ListOfMatch is one of:
+;; - empty
+;; - (cons Match ListOfMatch)
+;; interp. a list of matches between one team and another.
+(define LOM0 empty)
+(define LOM1 (list (make-match "Eugenie" "Maria")
+                   (make-match "Gabriela" "Nadia")))
+
+#;
+(define (fn-for-lom lom)
+  (cond [(empty? lom) (...)]
+        [else
+         (... (fn-for-match (first lom))
+              (fn-for-lom (rest lom)))]))
+
+
+
+
+;; CROSS PRODUCT OF TYPE COMMENTS TABLE
+;;  
+;;                                         lstb
+;;                               empty           (cons Player Roster)                
+;;                                           |
+;; l   empty                       true      |  false
+;; s                           --------------------------------
+;; t   (cons Player Roster)        false     |
+;; a                                         |(local [(define r1-empty? (empty? r1))
+;;                                                    (define r2-empty? (empty? r2))]
+;;                                                   (cond [(and r1-empty? r2-empty?) true]
+;;                                                         [(or r1-empty? r2-empty?) false]
+;;                                                         [else 
+;;                                                          (all-play? (rest r1) (rest r2))]))
+
+
+;; Roster Roster -> Boolean
+;; consumes two rosters, and produces true if
+;; all players on both teams will play if the teams play each other
+;; true if both are empty
+
+
+(check-expect (all-play? R0 R0) true)
+(check-expect (all-play? R0 R1) false)
+(check-expect (all-play? R1 R0) false)
+(check-expect (all-play? R1 R1) true)
+(check-expect (all-play? R1 R2) false)
+
+#;
+(define (all-play? r1 r2) true) ;stub
+
+;; template from Roster
+
+(define (all-play? r1 r2)
+  (local [(define r1-empty? (empty? r1))
+          (define r2-empty? (empty? r2))]
+    (cond [(and r1-empty? r2-empty?) true]
+          [(or r1-empty? r2-empty?) false]
+          [else 
+           (all-play? (rest r1) (rest r2))])))
+
+
+
+
+
+
+; Problem 2:
+; 
+; Now write a function that, given two teams, produces the list of tennis matches
+; that will be played. 
+; 
+; Assume that this function will only be called if the function you designed above
+; produces true. In other words, you can assume the two teams have the same number
+; of players. 
+; 
+
+
+;; Roster Roster -> ListOfMatch
+;; given two teams, produces the list of tennis matches that will be played
+;; assume same number of players
+
+(check-expect (list-matches R0 R0) empty)
+(check-expect (list-matches R1 R1) (list (make-match "Eugenie" "Eugenie")
+                                         (make-match "Gabriela" "Gabriela")
+                                         (make-match "Sharon" "Sharon")
+                                         (make-match "Aleksandra" "Aleksandra")))
+(check-expect (list-matches R2 R2) (list (make-match "Maria" "Maria")
+                                         (make-match "Nadia" "Nadia")
+                                         (make-match "Elena" "Elena")
+                                         (make-match "Anastasia" "Anastasia")
+                                         (make-match "Svetlana" "Svetlana")))
+#;
+(define (list-matches r1 r2) empty) ;stub
+
+
+;; template from Roster
+
+(define (list-matches r1 r2)
+  (cond [(empty? r1) empty]
+        [else 
+         (cons (make-match (first r1) (first r2))
+               (list-matches (rest r1) (rest r2)))]))
